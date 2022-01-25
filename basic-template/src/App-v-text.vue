efa684459e4ace7ec91d12cb90c6612c43285525
Jajahz <asamaporn.jajah@mail.kmutt.ac.th>
2022-01-25 10:48:02 +0700

    class 1

---
+++ basic-template/src/App-v-text.vue
@@ -0,0 +1,13 @@
+<script setup>
+const msg = 'Hello, Vue.js'
+const counter = 1
+</script>
+ 
+<template>
+<h1>{{ msg.toLowerCase() }}</h1>
+<p>{{ counter += 10 }}</p>
+</template>
+ 
+<style>
+
+</style>
---
+++ basic-template/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const headingStyle = "color: blue"
+const paraStyle = "text-decoration: underline"
+const imagePath = "../public/images/cheezelooker.jpg"
+</script>
+ 
+<template>
+<h1 v-bind:style = "headingStyle">INT203 Web Client</h1>
+<p :style="paraStyle">Asamaporn</p>
+<img src="./assets/logo.png">
+<img :src="imagePath" width="275" height="180">
+</template>
+ 
+<style>
+
+</style>

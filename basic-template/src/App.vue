27434fd54b9d6e0066e221e8bd08bc9f898f9bba
Jajahz <asamaporn.jajah@mail.kmutt.ac.th>
2022-02-01 10:41:05 +0700

    [add] lecture03

--- basic-template/src/App.vue
+++ basic-template/src/App.vue
@@ -1,58 +1,11 @@
 <script setup>
 
-const dataSet = [
-    {
-        name: 'salary',
-        amount: 10000
-    },
-    {
-        name: 'rent',
-        amount: -2500},
-    {
-        name: 'parking fee',
-        amount: 0
-    },
-    {
-        name: 'car fuel',
-        amount: -600
-    },
-    {
-        name: 'commission',
-        amount: 5500
-    }
-]
-
-function totalAmount(){
-    dataSet.forEach(i => {return i.amount})
-}
-
 </script>
  
 <template>
 
-<h1 style="text-align: center;">MY ACCOUNT</h1>
-
-<ul>
-
-    <li v-for="data in dataSet">
-
-    <p v-if="data.amount>0" style="color: green">
-    <p><b>Name:</b> {{data.name}}</p>
-    <b>Amount:</b> {{data.amount}}</p>
-
-    <p v-if="data.amount<0" style="color: red">
-    <p><b>Name:</b> {{data.name}}</p>
-    <b>Amount:</b> {{data.amount}}</p>
-
-    </li>
-    
-
-</ul>
-
-<p><b>Net Total</b> : {{totalAmount()}}</p>
-
 </template>
-
+ 
 <style>
 
 </style>
